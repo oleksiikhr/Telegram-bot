9a0bacfbc7ff296631ee3bf264b76c3f20f29994
Alexey Khrusch <alexeykhr@outlook.com>
2017-04-11 14:43:57 +0300

    fix DB

--- config/db.php
+++ config/db.php
@@ -25,7 +25,7 @@
 
 return [
     'driver'   => 'pgsql',
-    'host'     => 'ec2-54-225-242-74.compute-1.amazonaws.com:5432',
+    'host'     => 'ec2-54-225-242-74.compute-1.amazonaws.com',
     'database' => 'ddlmlukbiduagb',
     'username' => 'sklhxruujniuwz',
     'password' => '87825ad5266206271da51a71f87120358a7e4a7b18e08895ce78afccde965468',
