97ddd3b530044bcdac319a5ce79ffecd8c3f6388
Alexey Khrusch <alexeykhr@outlook.com>
2017-04-12 08:41:41 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -26,18 +26,19 @@ if (Parse::$text === '/start')
     // If ..
 
 	// \QB::table(DB::TABLE_USERS)->insert([
-	// 	'name' => Parse::$text
+	// 	'name'    => Parse::$fromID,
+	// 	'message' => Parse::$text
 	// ]);
 
-	$tlg->sendMessage(Parse::$fromID, "Выберите ник для своего персонажа");
+	$tlg->sendMessage(Parse::$fromID, "Введите ник для своего персонажа");
 
 	// Если ник занят, последнее сообщение в БД => /start
 }
 else
 {
     $k = Keyboard::replyKeyboardMarkup([
-        [KeyboardButton::new(Parse::$text . '1'), KeyboardButton::new(Parse::$text . '2')],
-        [KeyboardButton::new(Parse::$text . '3')]
+        [KeyboardButton::new(Parse::$text . '-1'), KeyboardButton::new(Parse::$text . '-2')],
+        [KeyboardButton::new(Parse::$text . '-3')]
     ]);
 
 	$tlg->sendMessage(Parse::$fromID, Parse::$text, null, null, $k);
